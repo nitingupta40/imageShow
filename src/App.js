e92bfc768e08a2a6e2719aad28fdb2de26837bb8
nitin gupta <nitingupta40@gmail.com>
2019-09-21 13:28:18 +1000

    changes
    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      "string" : "hi just testing",
+      "changeMe" : "Hi this is me",
+    } 
+  }
+
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Hi there</p>
+        <p>{this.state.string}</p>
+        <h1>{this.state.changeMe}</h1>
+        <button onClick={ () => this.setState({changeMe:"Hello World"})}>Chnage me</button>
+
+        <button onClick={ ()=> this.setState({string : "changed"})}>Chnage TExt</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
